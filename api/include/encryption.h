cc755ac84bb67eac8ed62c7565ca2819c9f5d3c2
wsilva6 <wsilva6@lenovo.com>
2024-02-05 09:41:19 -0300

    initial setup project

---
+++ api/include/encryption.h
@@ -0,0 +1,11 @@
+#ifndef ENCRYPTION_H
+#define ENCRYPTION_H
+
+#include <iostream>
+#include <string>
+
+namespace encryption {
+
+} // namespace encryption
+
+#endif // ENCRYPTION_H
---
+++ api/src/encryption.cpp
@@ -0,0 +1,8 @@
+#include "../include/encryption.h"
+#include <iostream>
+#include <fstream>
+#include <cctype>
+
+namespace crypto {
+
+} // namespace crypto

f3320e082dc36a59595c9130bce1cb5fd9396b10
wsilva6 <wildneif2@gmail.com>
2024-02-08 11:34:36 -0300

    react toastify implemented

--- views/src/App.js
+++ views/src/App.js
@@ -1,5 +1,7 @@
 import axios from "axios"
 import { useState } from "react";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const URL = "http://localhost:8080/";
 
@@ -8,14 +10,20 @@ function App() {
   const [responseData, setResponseData] = useState("")
 
   const postMessage = async () =>{
-    const response = await axios.post(URL, {
-      text: message
-    })
-    setResponseData(response.data.encryptedMessage)
-    setMessage("")
+    try {
+      const response = await axios.post(URL, {
+        text: message
+      })
+      setResponseData(response.data.encryptedMessage)
+      toast.success("Messagem Criptografada com sucesso!")
+      setMessage("")
+    } catch (error) {
+      toast.error("Erro ao Criptografar messagem!")
+    }
   }
   return (
     <div className="App w-screen min-h-screen bg-slate-800 flex justify-center items-center">
+      <ToastContainer position="bottom-left" autoClose={3000}/>
       <form className="w-3/12 h-96 rounded-2xl bg-slate-900 flex justify-center items-center text-lg text-white flex-col gap-3" onSubmit={(e) =>{
         e.preventDefault();
         postMessage();
@@ -26,6 +34,7 @@ function App() {
         <button type="submit" className="rounded-md border-white border bg-slate-900 hover:bg-slate-950 duration-500 ease-in-out px-3 py-2">Criptografar</button>
         <div className="text-lg text-white">{responseData}</div>
       </form>
+    
     </div>
   );
 }
